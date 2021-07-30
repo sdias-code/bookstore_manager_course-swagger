6705832d03e0f065cd8b2e19e29d2dfce0a30579
sdias-code <silviodias.ms@gmail.com>
2021-07-30 10:04:22 -0400

    Atualizado SwaggerConfigurations

--- src/main/java/com/sdias/bookstoremanager/config/swagger/SwaggerConfigurations.java
+++ src/main/java/com/sdias/bookstoremanager/config/swagger/SwaggerConfigurations.java
@@ -14,7 +14,7 @@ public class SwaggerConfigurations {
     public Docket bookstoremanagerApi(){
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.sdias.bookstoremanager"))
+                .apis(RequestHandlerSelectors.basePackage("com.sdias.bookstoremanager")) //meu caminho padrao
                 .paths(PathSelectors.ant("/**"))
                 .build();
     }
